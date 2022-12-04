27db61b77c0d175144c94bb162d778d0aaf517a7
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-12-04 18:47:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int sum = 0;
+            
+
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                sum += arr[ i ];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
